8f5b0953578e462676c86cdcdbb7189629222f0e
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-07-01 11:15:12 +0300

    Add files via upload

---
+++ WorkingWithAbstractionLab/hotelReservation/DayOfWeek.java
@@ -0,0 +1,22 @@
+package WorkingWithAbstractionLab.hotelReservation;
+
+public enum DayOfWeek {
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6),
+    SUNDAY(7);
+
+    private int dayNumber;
+
+    private DayOfWeek(int dayNumber) {
+        this.dayNumber = dayNumber;
+    }
+
+    public int getDayNumber() {
+        return dayNumber;
+    }
+
+}
