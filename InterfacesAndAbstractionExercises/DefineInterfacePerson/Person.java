2e746212df29f5aa8fb5cbc9042026705dad5184
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-07-12 17:22:11 +0300

    Add files via upload

---
+++ InterfacesAndAbstractionExercises/DefineInterfacePerson/Person.java
@@ -0,0 +1,7 @@
+package InterfacesAndAbstractionExercises.DefineInterfacePerson;
+
+public interface Person {
+    String getName();
+    int getAge();
+
+}
---
+++ InterfacesAndAbstractionExercises/car.java
@@ -0,0 +1,4 @@
+package InterfacesAndAbstractionExercises;
+
+public class car {
+}
